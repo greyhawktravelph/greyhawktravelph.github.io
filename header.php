fbb7b3e14a3fc4134cbb59f9e45f2723d0d90e86
greyhawktravelph <greyhawktravelph@gmail.com>
2020-06-21 17:21:31 +0800

    Update header.php

--- header.php
+++ header.php
@@ -1,8 +1,16 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
+<html lang="en">
   <head>
     <meta charset="utf-8">
     <title>Greyhawk Travel and Tours</title>
   </head>
   <body>
-    
+    <header>
+      <nav>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About Us</a></li>
+          <li><a href="">Contact Us</a></li>
+        </ul>
+      </nav>
+    </header>
